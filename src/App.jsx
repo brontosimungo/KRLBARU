15a09c49ba9bb243f89380687e1c9e33240cb7dd
brontosimungo <brontosimungo@outlook.co.id>
2025-05-24 15:59:49 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TrainMap from './components/TrainMap';
+
+export default function App() {
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <TrainMap />
+    </div>
+  );
+}
